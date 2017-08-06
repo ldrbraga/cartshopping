e60fc29163d153d5c5219637fd7b7e068419dc2d
ldrbraga <ldrbraga@gmail.com>
2017-08-05 23:02:47 -0300

    alteração no index

--- app/controllers/apiController.js
+++ app/controllers/apiController.js
@@ -1,5 +1,5 @@
 app.controller('apiController', function ($scope, $http) {
-    $http.get('http://api.intercase.net.br/items.json').success(function(data){
-        console.log(data);
+    $http.get('http://api.intercase.net.br/items.json').success(function(items){
+        console.log(items);
     });
 });
